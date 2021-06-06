3608a62397feef66c32892c7c696558ba005988c
Pedro Marcelo <pedromarcelodesaalves@gmail.com>
2021-06-05 21:00:11 -0300

    feat(database): adicionar conexão da API com o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        `.env.${process.env.NODE_ENV}.local`,
+        `.env.${process.env.NODE_ENV}`,
+        '.env.local',
+        '.env',
+      ],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          uri: config.get('DATABASE_URI'),
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          authSource: 'admin',
+        };
+      },
+    }),
+  ],
 })
 export class AppModule {}
